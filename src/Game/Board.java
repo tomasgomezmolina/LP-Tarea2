942b1fdcec6575b46a18b06fc1c70c6743d7e828
aneumann20 <anita.neumann.14@sansano.usm.cl>
2015-12-12 18:45:34 -0300

    las piezas se mueven bien(en diagonal etc) y no se salen del tablero, fata que se coman, esta en proceso

--- src/Game/Board.java
+++ src/Game/Board.java
@@ -1,7 +1,4 @@
 package Game;
-
-
-import lp.motor.Context;
 import lp.motor.MouseHandler;
 import java.awt.*;
 import java.util.ArrayList;
@@ -9,13 +6,17 @@ import java.util.ArrayList;
 
 public class Board {
 
+    int identificador;
+    int clics = 0;
+    int turno = 0;
+    Piece found;
+
     private static ArrayList<Point> bloques = new ArrayList<>();
 
     public static ArrayList<Point> getBloques() {
         return bloques;
     }
 
-    //public ArrayList<Piece> pieces = new ArrayList<>();
 
     public void drawBoard(Graphics graphics){
         for (int i = 50 ; i < 550 ; i += 50) {
@@ -27,13 +28,13 @@ public class Board {
 
                     }
                     else {
-                        graphics.setColor(Color.pink);
+                        graphics.setColor(Color.lightGray);
                         graphics.fillRect(j, i, 50, 50);
                     }
                 }
                 else{
                     if ((j / 50) % 2 == 0) {
-                        graphics.setColor(Color.pink);
+                        graphics.setColor(Color.lightGray);
                         graphics.fillRect(j, i, 50, 50);
                     }
                     else {
@@ -49,17 +50,54 @@ public class Board {
         }
     }
 
-   /* public Piece getPieceClicked() {
-        for (Piece piece : pieces)
-            if (piece.wasClicked())
-                return piece;
-
-        return null;
+    public void movePiece(MouseHandler mouseHandler, Point point, ArrayList<Piece> pieces1, ArrayList<Piece> pieces2) {
+        if (turno % 2 == 0) { // turno equipo 1
+            if (clics % 2 == 0) {
+                for (Piece pieza : pieces1) { // ((point.x/y / 50) * 50 + 5) -> hace que el punto se aproxime a las coordenadas de las piezas
+                    if (((point.x / 50) * 50 + 5) == pieza.getX() && ((point.y / 50) * 50 + 5) == pieza.getY() && mouseHandler.isButtonPressed()) {
+                        found = pieza;
+                        clics++;
+                        break;
+                    }
+                }
+            } else if (mouseHandler.isButtonJustPressed()) {
+                if (found.sonIguales(point, pieces1, pieces2)) { // para no ponerse sobre otra ficha
+                    if (((point.x / 50) * 50 + 5) < 550 && 50 < ((point.x / 50) * 50 + 5) && ((point.y / 50) * 50 + 5) < 550 && 50 < ((point.y / 50) * 50 + 5)) { //restriccion para no salir del tablero
+                        if ( (((((point.x / 50) * 50 + 5) == (found.getX() - 50)) || ((point.x / 50) * 50 + 5) == (found.getX() + 50))
+                                && (((point.y / 50) * 50 + 5) == (found.getY() + 50)) )
+                                || (found.preComer(point, found, pieces1, pieces2, turno))) { // restriccion para avanzar en diagonal y poder "comer"
+                            found.setX((point.x / 50) * 50 + 5);
+                            found.setY((point.y / 50) * 50 + 5);
+                            clics++;
+                            turno++;
+                        }
+                    }
+                }
+            }
+        } else {           // turno equipo 2
+            if (clics % 2 == 0) {
+                for (Piece pieza : pieces2) {
+                    if (((point.x / 50) * 50 + 5) == pieza.getX() && ((point.y / 50) * 50 + 5) == pieza.getY() && mouseHandler.isButtonPressed()) {
+                        found = pieza;
+                        clics++;
+                    }
+                }
+            } else if (mouseHandler.isButtonJustPressed()) {
+                if (found.sonIguales(point, pieces1, pieces2)) {
+                    if (((point.x / 50) * 50 + 5) < 550 && 50 < ((point.x / 50) * 50 + 5) && ((point.y / 50) * 50 + 5) < 550 && 50 < ((point.y / 50) * 50 + 5)) {
+                        if ( (((((point.x / 50) * 50 + 5) == (found.getX() - 50)) || ((point.x / 50) * 50 + 5) == (found.getX() + 50))
+                                && (((point.y / 50) * 50 + 5) == (found.getY() - 50)))
+                                || (found.preComer(point, found, pieces1, pieces2, turno))) {
+                            found.setX(((point.x / 50) * 50 + 5));
+                            found.setY((point.y / 50) * 50 + 5);
+                            clics++;
+                            turno++;
+                        }
+                    }
+                }
+            }
+        }
     }
 
-    public void updatePieceClicked(MouseHandler mouseHandler) {
-        for (Piece piece : pieces)
-            piece.setClicked(mouseHandler);
 
-    }*/
 }
--- src/Game/Main.java
+++ src/Game/Main.java
@@ -4,117 +4,68 @@ import lp.motor.Application;
 import lp.motor.Context;
 import lp.motor.MouseHandler;
 
-
 import java.awt.*;
 import java.util.ArrayList;
 
-public class Main implements Context
-{
+public class Main implements Context {
     Board board;
     Piece piece1;
     Piece piece2;
-    Piece piece3;
-    ArrayList<Piece> pieces;
-    Piece currentPiece = null;
-    int identificador;
-    int clics = 0;
+    ArrayList<Piece> pieces1;
+    ArrayList<Piece> pieces2;
+    /*Vector<Piece>*/
 
 
-    public Main()
-    {
-        pieces = new ArrayList<>();
+    public Main() {
+        pieces1 = new ArrayList<>();
+        pieces2 = new ArrayList<>();
         board = new Board();
         int cont = 0;
         int cont2 = 20;
-        for (int i = 0; i <= 200; i=i+50) {
-            for (int j = 0; j <= 3 ; j++) {
-                piece1 = new Piece(55+2*i+(1+j)%2*50 , 55+50*j, 1, j+cont);
-                pieces.add(piece1);
+        for (int i = 0; i <= 200; i = i + 50) {
+            for (int j = 0; j <= 3; j++) {
+                piece1 = new Piece(55 + 2 * i + (1 + j) % 2 * 50, 55 + 50 * j, 1, j + cont);
+                pieces1.add(piece1);
             }
             cont += 4;
         }
-        for (int i = 0; i <= 200 ; i=i+50) {
-            for (int j = 0; j <=3 ; j++) {
-                piece2 = new Piece(55+2*i+(1+j)%2*50 , 355+50*j , 2, j+cont2);
-                pieces.add(piece2);
+        for (int i = 0; i <= 200; i = i + 50) {
+            for (int j = 0; j <= 3; j++) {
+                piece2 = new Piece(55 + 2 * i + (1 + j) % 2 * 50, 355 + 50 * j, 2, j + cont2);
+                pieces2.add(piece2);
             }
             cont2 += 4;
         }
-        //piece3 = new Piece(55, 55, 1, 21);
-        // aquí puede inicializar valores y crear los objetos de juego.
     }
 
     @Override
-    public void update(MouseHandler mouseHandler)
-    {
+    public void update(MouseHandler mouseHandler) {
         Point point = mouseHandler.getMousePosition();
 
-        /*currentPiece = board.getPieceClicked();
+        board.movePiece(mouseHandler, point, pieces1, pieces2);
 
-        board.updatePieceClicked(mouseHandler);
-
-        if(currentPiece != null) {
-
-        } */
-
-        if (clics%2 == 0) {
-            for (Piece pieza : pieces) {
-                if (((point.x / 50) * 50 +5)== pieza.getX() && ((point.y / 50) * 50+ 5) == pieza.getY() && mouseHandler.isButtonPressed()) {
-                    identificador = pieza.getIdentificador();
-                    clics++;
-                    System.out.println(identificador);
-                    System.out.println("chao");
-                }
-            }
-        }
-        else if(clics%2 == 1) {
-            for (Piece pieza : pieces){
-                int id = pieza.getIdentificador();
-                System.out.println(id);
-                if (id == identificador ) {
-                    System.out.println("Hola");
-                    pieza.setX((point.x / 50) * 50 + 5);
-                    System.out.println(pieza.getX());
-                    pieza.setY((point.y / 50) * 50 + 5);
-                    clics++;
-                }
-            }
         }
 
 
-
-
-
-        // aquí actualice sus objetos para que puedan interactuar con input de usuario o entre los mismos
-        // objetos.
-
-        // por ejemplo imprimir algo si el mouse está cerca de la esquina superior izquierda:
-       //Point point = mouseHandler.getMousePosition();
-        //if (point.x < 200 && point.y < 200)
-          //  System.out.println(point);
-    }
 
     @Override
-    public void render(Graphics graphics)
-    {
+    public void render(Graphics graphics) {
         // aquí, y solo aquí, puede dibujar cosas en la pantalla.
 
         // por ejemplo dibujar un círculo verde:
         board.drawBoard(graphics);
 
-        for (Piece pieza:pieces) {
+        for (Piece pieza : pieces1) {
+            pieza.drawPiece(graphics);
+        }
+        for (Piece pieza : pieces2) {
             pieza.drawPiece(graphics);
         }
 
 
-        if (currentPiece != null) {
-
-        }
-        //piece3.drawPiece(graphics);
     }
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         // el método main solo se encargará de iniciar el sistema.
         Application.start(600, 600, "Elepé!", 60, new Main());
     }
